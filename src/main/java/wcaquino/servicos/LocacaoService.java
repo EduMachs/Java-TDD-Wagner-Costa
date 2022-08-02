30701e9d5019745e33580e96ddfd5e854f0bdce9
Eduardo de Oliveira Machado <80400278@pucrs.br>
2022-08-02 18:07:36 -0300

    Following through the JUnit classes

---
+++ src/main/java/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,30 @@
+package wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import wcaquino.entidades.Filme;
+import wcaquino.entidades.Locacao;
+import wcaquino.entidades.Usuario;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+		
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+}
---
+++ src/test/java/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,40 @@
+package wcaquino.servicos;
+
+
+
+import static wcaquino.utils.DataUtils.isMesmaData;
+import static wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.Date;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import wcaquino.entidades.Filme;
+import wcaquino.entidades.Locacao;
+import wcaquino.entidades.Usuario;
+
+public class LocacaoServiceTest {
+
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
+	@Test
+	public void testeLocacao() {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//verificacao
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+	}
+}
